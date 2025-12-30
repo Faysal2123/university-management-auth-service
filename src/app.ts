1b3fc502cd437e387153fe1f61b226010b3847b2
Faysal2123 <foysal2005n1@gmail.com>
2025-12-31 00:53:55 +0600

    test

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
 import express, { type Application, type Request, type Response } from 'express'
-const app:Application = express()
+const app: Application = express()
 import cors from 'cors'
 
 app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req:Request, res:Response) => {
+app.get('/', (req: Request, res: any) => {
   res.send('Hello World!')
 })
 
-export default app;
+export default app
