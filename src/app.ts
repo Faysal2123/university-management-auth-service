811f1a2acfc38080652c30b582a082a6becdb4a2
Faysal2123 <foysal2005n1@gmail.com>
2025-12-30 19:36:06 +0600

    [DONE] setup express, cors, parser

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express, { type Application, type Request, type Response } from 'express'
+const app:Application = express()
+import cors from 'cors'
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.get('/', (req:Request, res:Response) => {
+  res.send('Hello World!')
+})
+
+export default app;
