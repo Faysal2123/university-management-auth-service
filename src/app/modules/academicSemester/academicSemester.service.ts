9ca9f402e66d5007c67989ecac7612ba07b5cc86
Faysal2123 <foysal2005n1@gmail.com>
2026-02-11 21:25:59 +0600

    feat: add duplicate semester check and title-code validation

--- src/app/modules/academicSemester/academicSemester.service.ts
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -1,9 +1,14 @@
+import ApiError from '../../../errors/ApiError.js'
+import { academicSemesterTitleCodeMapper } from './academicSemester.constant.js'
 import type { IAcademicSemester } from './academicSemester.interface.js'
 import { AcademicSemester } from './academicSemesterModel.js'
-
+import httpStatus from 'http-status'
 const createSemester = async (
   payload: IAcademicSemester,
 ): Promise<IAcademicSemester> => {
+  if (academicSemesterTitleCodeMapper[payload.title] !== payload.code) {
+    throw new ApiError(httpStatus.BAD_REQUEST, 'Invalid Semester code')
+  }
   const result = await AcademicSemester.create(payload)
   return result
 }
--- src/app/modules/academicSemester/academicSemesterModel.ts
+++ src/app/modules/academicSemester/academicSemesterModel.ts
@@ -6,6 +6,8 @@ import {
   academicSemesterMonths,
   academicSemesterTitles,
 } from './academicSemester.constant.js'
+import httpStatus from 'http-status'
+import ApiError from '../../../errors/ApiError.js'
 
 const academicSemesterSchema = new Schema<IAcademicSemester>(
   {
@@ -38,6 +40,15 @@ const academicSemesterSchema = new Schema<IAcademicSemester>(
     timestamps: true,
   },
 )
+academicSemesterSchema.pre('save', async function () {
+  const isExist = await AcademicSemester.findOne({
+    title: this.title,
+    year: this.year,
+  })
+  if (isExist) {
+    throw new ApiError(httpStatus.CONFLICT, 'Academic Semester already exists')
+  }
+})
 
 export const AcademicSemester = model<IAcademicSemester, AcademicSemesterModel>(
   'AcademicSemester',
