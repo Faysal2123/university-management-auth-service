0440f1f04df7086514f9bd850c7535534dbf625a
Faysal2123 <foysal2005n1@gmail.com>
2026-02-14 23:44:13 +0600

    add sendResponse utility for standardized API responses

--- src/app/modules/academicSemester/academicSemester.controller.ts
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -1,12 +1,15 @@
 import { AcademicSemesterService } from './academicSemester.service.js'
 import catchAsync from '../../../shared/catchAsync.js'
-
+import sendResponse from '../../../shared/sendResponse.js'
+import httpStatus from 'http-status'
 const createSemester = catchAsync(async (req, res, next) => {
   const { ...academicSemesterData } = req.body
   const result =
     await AcademicSemesterService.createSemester(academicSemesterData)
   next()
-  res.status(200).json({
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
     success: true,
     message: 'Academic Semester created successfully',
     data: result,
--- src/app/modules/users/user.controller.ts
+++ src/app/modules/users/user.controller.ts
@@ -1,11 +1,15 @@
 import { UserService } from './user.service.js'
 import catchAsync from '../../../shared/catchAsync.js'
+import sendResponse from '../../../shared/sendResponse.js'
+import httpStatus from 'http-status'
 
 const createUser = catchAsync(async (req, res, next) => {
   const { user } = req.body
   const result = await UserService.createUser(user)
   next()
-  res.status(200).json({
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
     success: true,
     message: 'User created successfully',
     data: result,
